770b3ed36a8422ea97c59a91ad5986541f5c279d
hosamsam <hosamelnabawy1996@gmail.com>
2017-09-20 16:51:04 +0200

    add push & network plugins

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -48,6 +48,8 @@ export class HomePage {
 
     console.log('%s%c%s', 'component class name we are using now ', 'color: green;font-weight:bold', this.navCtrl.getActive().id);
 
+    console.log(`You are ${this.appUtils.IsConnected?'':'not'} connected to the internet`)
+
   }
 
 
