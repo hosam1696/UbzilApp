63efa120ab150e5191f6e579ef547954d85897df
hosamsam <hosamelnabawy1996@gmail.com>
2017-09-25 18:07:22 +0300

    # modify app styles and pages

--- src/pages/requests-detail/requests-detail.ts
+++ src/pages/requests-detail/requests-detail.ts
@@ -1,3 +1,4 @@
+import { AppUtilFunctions } from './../../app/appglobal/app.utilfuns';
 // Main Components
 import {Component} from '@angular/core';
 import {IonicPage, NavController, ModalController, NavParams, Events, ActionSheetController, ToastController} from 'ionic-angular';
@@ -22,81 +23,30 @@ export class RequestsDetail {
     myRequests: any;
     closetext:any;
     toasttext:any;
-    
+    isRTL: boolean;
 
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
         public events: Events,
-        public translate: TranslateService,
-        public actionSheetCtrl: ActionSheetController,
         public modalCtrl: ModalController,
-        private toastCtrl: ToastController
+        public appUtils: AppUtilFunctions
     ) {
         this.myRequests = "mine";
     }
 
     ionViewDidEnter() {
         // Run After Page Already Entered
+        this.isRTL = this.appUtils.isRTL
     }
 
     ionViewDidLoad() {
-        // Run After Page Already Loaded
-        this.translate.get('User-was-choosed')
-            .subscribe(lang => {
-                this.toasttext = lang;
-            })
-        this.translate.get('Close')
-            .subscribe(lang => {
-                this.closetext = lang;
-            })
+        // Run After Page Already Loade
 
     }
-    goProfilePage() {
-        this.navCtrl.push('ProfilePage');
-    }
-    presentToast() {
-        let toast = this.toastCtrl.create({
-            message: this.toasttext,
-            duration: 33000,
-            showCloseButton: true,
-            closeButtonText: this.closetext,
-            position: 'middle'
-        });
+   
 
-        toast.onDidDismiss(() => {
-            console.log('Dismissed toast');
-        });
-
-        toast.present();
-    }
-
-    applyjop() {
-        let applyjopModal = this.modalCtrl.create('ApplyJop');
-        applyjopModal.present();
-        applyjopModal.onDidDismiss(data => {
-            console.log(data);
-        });
-    }
-
-    locationmodal() {
-        let getlocationModal = this.modalCtrl.create(GetLocation);
-        getlocationModal.present();
-        getlocationModal.onDidDismiss(data => {
-            // Saving this info to local storage after updating user profile info
-        })
-    }
-    MessagesDetail() {
-        let MessagesDetailModal = this.modalCtrl.create(MessagesDetail);
-        MessagesDetailModal.present();
-        MessagesDetailModal.onDidDismiss(data => {
-            // Saving this info to local storage after updating user profile info
-        })
-
-    }
-
-
-    protected navigateTo(page: string, isModal: boolean = false, pageData?: any): void {
+    navigateTo(page: string, isModal: boolean = false, pageData?: any): void {
         if (isModal) {
             let EditProfileModal = this.modalCtrl.create(page, { pageData });
             EditProfileModal.present();
