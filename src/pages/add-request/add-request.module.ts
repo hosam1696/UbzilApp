934828ccaf82405c9edaee72d84db57b76eb8473
hosamsam <hosamelnabawy1996@gmail.com>
2017-10-10 13:52:25 +0200

    build form for checkbox inputs

--- src/pages/add-request/add-request.module.ts
+++ src/pages/add-request/add-request.module.ts
@@ -2,6 +2,7 @@ import { TranslateModule } from 'ng2-translate';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AddRequest } from './add-request';
+import { CheckboxControlValueAccessor } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- src/pages/add-request/add-request.ts
+++ src/pages/add-request/add-request.ts
@@ -1,11 +1,11 @@
-import { AppUtilFunctions } from './../../app/appglobal/app.utilfuns';
-import { ServicesProvider } from './../../providers/services/services';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events, ModalController } from 'ionic-angular';
+import {AppUtilFunctions} from './../../app/appglobal/app.utilfuns';
+import {ServicesProvider} from './../../providers/services/services';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, Events, ModalController} from 'ionic-angular';
 
 import {GetLocation} from "../get-location/get-location";
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { ILocalUser } from '../../app/appglobal/app.interfaces';
+import {FormGroup, FormBuilder, Validators, FormControl, FormArray} from '@angular/forms';
+import {ILocalUser} from '../../app/appglobal/app.interfaces';
 
 
 @IonicPage()
@@ -20,20 +20,19 @@ export class AddRequest {
   DataFromModal: any = {};
   RequestForm: FormGroup;
   localUser: ILocalUser;
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public modalCtrl: ModalController,
-     public appUtils:AppUtilFunctions,
-     public events: Events,
-    public services: ServicesProvider,
-    formBuilder: FormBuilder
-  ) {    
+  FormsShapes: any[];
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public modalCtrl: ModalController,
+              public appUtils: AppUtilFunctions,
+              public events: Events,
+              public services: ServicesProvider,
+              formBuilder: FormBuilder) {
 
     this.pageParams = this.navParams.get('pageData');
 
     console.log('params data from sub category page', this.pageParams);
-    
+
     this.RequestForm = formBuilder.group({
       title: ['', Validators.required],
       address: ['', Validators.required],
@@ -43,102 +42,136 @@ export class AddRequest {
 
   }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
 
-    
-    
+
   }
-  
+
   async ionViewDidLoad() {
     // Run After Page Already Loaded
-    
+
     this.localUser = await this.appUtils.getUserInfo();
-    
+
     this.services.getServiceFormShape(
       {
-        "user_id":  3 ,
-        "verifycode": this.localUser.verifycode || "$2y$12$XQBdOjshGvoSRcT6uTlJaOkOiV.htMTyyT09IXxdjHrSQeoc/vgkO", "lang_code": this.pageParams.lang_code,
+        "user_id": 3,
+        "verifycode": this.localUser.verifycode || "$2y$12$XQBdOjshGvoSRcT6uTlJaOkOiV.htMTyyT09IXxdjHrSQeoc/vgkO",
+        "lang_code": this.pageParams.lang_code,
         "service_id": this.pageParams.id,
       }
     ).subscribe(({status, data, error}) => {
       if (status === 'success') {
+
         let groupByType = type => data.forms.filter(x => x.type == type);
+        
         let currentFormInputs = ['select', 'input', 'textarea', 'checkbox', 'file'];
 
         console.log(data.forms);
 
-        
+        this.FormsShapes = data.forms;
+
         for (let input of currentFormInputs) {
           if (groupByType(input).length > 0) {
-            this.newFormDs[input] = groupByType(input)
-            this.newFormDs[input].forEach(inputControl => {
-              
-              let forms = this.RequestForm.get('forms') as FormGroup;
 
-              forms.addControl(inputControl.id,new FormControl('', Validators.required))
+            this.newFormDs[input] = groupByType(input)
+            
+            this.newFormDs[input].forEach(inputControl => {
+
+              
+
+              let forms = this.RequestForm.get('forms') as FormGroup;
+              if (input === 'checkbox') {
+                let fillCheckControls = ()=>{
+
+                  let controlsArray = {};
+                  for (let num of inputControl.sub) {
+                    controlsArray[num.id] = new FormControl(false);
+                  }
+                  return controlsArray;
+
+                }
+                forms.addControl(inputControl.id, new FormGroup(fillCheckControls()))
+              }
+              else {
+
+                forms.addControl(inputControl.id, new FormControl('', Validators.required))
+              }
 
             });
           }
         }
 
         console.log('new Form Data structure', this.newFormDs);
-        
+
       } else {
         console.warn(error)
-        }
+      }
     });
 
 
-    // For Development Only 
+    // For Development Only
 
     this.sendRequest();
 
   }
 
-    locationmodal() {
-      let getlocationModal = this.modalCtrl.create('GetLocation', { pageData: { lat: '30.4446546', lng: '20.484897', service_id: this.pageParams.id, skipSearch: true  }});
-        getlocationModal.present();
-        getlocationModal.onDidDismiss(data => {
-            // Saving this info to local storage after updating user profile info
-        })
-    }
+  locationmodal() {
+    let getlocationModal = this.modalCtrl.create('GetLocation', {
+      pageData: {
+        lat: '30.4446546',
+        lng: '20.484897',
+        service_id: this.pageParams.id,
+        skipSearch: true
+      }
+    });
+    getlocationModal.present();
+    getlocationModal.onDidDismiss(data => {
+      // Saving this info to local storage after updating user profile info
+    })
+  }
 
 
-  
-    openModal() {
-      let modal = this.modalCtrl.create('PlacesModalPage');
+  openModal() {
+    let modal = this.modalCtrl.create('PlacesModalPage');
 
-      modal.onDidDismiss(data => {
-        console.log('data from modal', data);
+    modal.onDidDismiss(data => {
+      console.log('data from modal', data);
+      if (data.governorate || data.city) {
         this.DataFromModal = data;
-        this.MreqAddress = data.governorate + (data.city ? ' - ' + data.city  + (data.district ? ' - ' + data.district:''): '');
-        this.RequestForm.get('address').setValue(data.governorate + (data.city ? ' - ' + data.city  + (data.district ? ' - ' + data.district:''): ''));
+        this.MreqAddress = data.governorate + (data.city ? ' - ' + data.city + (data.district ? ' - ' + data.district : '') : '');
+        this.RequestForm.get('address').setValue(data.governorate + (data.city ? ' - ' + data.city + (data.district ? ' - ' + data.district : '') : ''));
+      
+      }
       });
 
-      modal.present()
-    }
-  
-  
-  
-     sendRequest() {
-    
-
-       let requesrDetails = {
-         service_id: this.pageParams.id,
-         request_title: 'service title',
-         longitude: this.localUser.longitude,
-         latitude: this.localUser.latitude,
-         country_id: this.DataFromModal.country_id || 45,
-         city_id: this.DataFromModal.city_id || 65,
-         district_id: this.DataFromModal.district_id || 56,
-         user_id: this.localUser.id,
-         ...this.RequestForm.value
-       };
+    modal.present()
+  }
 
 
-       console.log('almost request data',requesrDetails, this.RequestForm);
+  sendRequest() {
 
-       this.RequestForm.valid?(console.info('Your Form is Valid')):(console.warn('You form Errors', this.RequestForm.errors))
-    }
-  
+
+    let requesrDetails = {
+      service_id: this.pageParams.id,
+      order_title: 'service title',
+      longitude: this.localUser.longitude,
+      latitude: this.localUser.latitude,
+      country_id: this.DataFromModal.country_id || 45,
+      city_id: this.DataFromModal.city_id || 65,
+      district_id: this.DataFromModal.district_id || 56,
+      user_id: this.localUser.id,
+      sector_id: 15,
+      ...this.RequestForm.value
+    };
+
+
+    console.log('almost request data', requesrDetails, this.RequestForm);
+
+    this.RequestForm.valid ? (console.info('Your Form is Valid')) : (console.warn('You form Errors', this.RequestForm.errors))
+  }
+
+  doClick(option) {
+    console.log('do click', option)
+  }
+
 }
